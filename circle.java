35b0f65b848e19cb7fb77e0819716254ceb15053
Komal Singh <115030881+Komalsingh1512@users.noreply.github.com>
2026-02-26 23:55:14 +0530

    second commit

---
+++ circle.java
@@ -0,0 +1,7 @@
+public class circle {
+    public static void main (String[] args){
+        double radius = 5.0;
+        double area = Math.PI * Math.pow(radius, 2);
+        System.out.println("The area of the circle with radius " + radius + " is: " + area);
+    }
+}
