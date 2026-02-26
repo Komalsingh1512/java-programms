a6e87e8022da3aa36e05425c29760a6fa89f0eea
Komal Singh <115030881+Komalsingh1512@users.noreply.github.com>
2026-02-26 23:56:04 +0530

    second commit

---
+++ triangle.java
@@ -0,0 +1,8 @@
+public class triangle {
+    public static void main (String[] args){
+        double base = 10.0;
+        double height = 5.0;
+        double area = 0.5 * base * height;
+        System.out.println("The area of the triangle with base " + base + " and height " + height + " is: " + area);
+    }
+}
